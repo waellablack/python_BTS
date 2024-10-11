779ea07518c08420a665d6db6d3cd2af70f077f7
waellablack <waellablackpro@gmail.com>
2024-10-11 10:57:14 +0200

    Update coordonees.py

--- chap5_fonction/coordonees.py
+++ chap5_fonction/coordonees.py
@@ -1,16 +1,16 @@
  def control_bande_passante(debit_reseau, seuil_reseau):
-#     while True:
-#         try:
-#             latitude = float(input("Entrez la latitude (-90 à 90) : "))
-#             longitude = float(input("Entrez la longitude (-180 à 180) : "))
+    while True:
+        try:
+            latitude = float(input("Entrez la latitude (-90 à 90) : "))
+            longitude = float(input("Entrez la longitude (-180 à 180) : "))
             
-#             if -90 <= latitude <= 90 and -180 <= longitude <= 180:
-#                 print("Coordonnées valides : Latitude = {}, Longitude = {}".format(latitude, longitude))
-#             else:
-#                 print("Coordonnées invalides. La latitude doit être entre -90 et 90, et la longitude entre -180 et 180.")
-#                 break  
-#         except :
-#             print("Erreur : Veuillez entrer des nombres valides pour la latitude et la longitude.")
-#             break  
+            if -90 <= latitude <= 90 and -180 <= longitude <= 180:
+                print("Coordonnées valides : Latitude = {}, Longitude = {}".format(latitude, longitude))
+            else:
+                print("Coordonnées invalides. La latitude doit être entre -90 et 90, et la longitude entre -180 et 180.")
+                break  
+        except :
+            print("Erreur : Veuillez entrer des nombres valides pour la latitude et la longitude.")
+            break  
 
-# coordonnees_gps()
+coordonnees_gps()
